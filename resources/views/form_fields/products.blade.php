2fd0aef00e1c9b1eb202b97eb7cbb12d1a1ded16
Jade Geels <jade@justbetter.nl>
2025-09-25 13:31:26 +0200

    Move v-if to fix v-else

--- resources/views/form_fields/products.blade.php
+++ resources/views/form_fields/products.blade.php
@@ -1,32 +1,34 @@
 <quote-data v-slot="quoteData">
     <div>
-        <x-rapidez::productlist
-            v-if="quoteData.products.length"
-            value="quoteData.products.map(product => product.sku)"
-            field="sku"
-        >
-            <x-slot:items>
-                <div v-for="item in items" class="flex gap-5 items-center">
-                    <button type="button" v-on:click="confirm('@lang('Are you sure you want to remove this product?')') && quoteData.removeProduct(item.sku)">
-                        <x-heroicon-o-x-circle class="size-6"/>
-                    </button>
-                    <div>
-                        <img
-                            v-if="item.thumbnail"
-                            class="object-contain h-16 w-20 shrink-0"
-                            :alt="item.name"
-                            :src="`/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product${item.thumbnail}.webp`"
-                        />
-                        <x-rapidez::no-image v-else />
+        <template v-if="quoteData.products.length">
+            <x-rapidez::productlist
+                value="quoteData.products.map(product => product.sku)"
+                field="sku"
+            >
+                <x-slot:items>
+                    <div v-for="item in items" class="flex gap-5 items-center">
+                        <button type="button" v-on:click="confirm('@lang('Are you sure you want to remove this product?')') && quoteData.removeProduct(item.sku)">
+                            <x-heroicon-o-x-circle class="size-6"/>
+                        </button>
+                        <div>
+                            <img
+                                v-if="item.thumbnail"
+                                class="object-contain h-16 w-20 shrink-0"
+                                :alt="item.name"
+                                :src="`/storage/{{ config('rapidez.store') }}/resizes/200/magento/catalog/product${item.thumbnail}.webp`"
+                            />
+                            <x-rapidez::no-image v-else />
+                        </div>
+                        <div class="flex flex-col">
+                            <span>@{{ quoteData.from(item.sku).qty }} x @{{ item.name }}</span>
+                            <template v-for="option in quoteData.from(item.sku).options ?? {}">
+                                <span class="text-muted">@{{ option }}</span>
+                            </template>
+                        </div>
                     </div>
-                    <div class="flex flex-col">
-                        <span>@{{ quoteData.from(item.sku).qty }} x @{{ item.name }}</span>
-                        <template v-for="option in quoteData.from(item.sku).options ?? {}">
-                            <span class="text-muted">@{{ option }}</span>
-                        </template>
-                    </div>
-                </div>
-            </x-slot:items>
+                </x-slot:items>
+            </x-rapidez::productlist>
+        </template>
         </x-rapidez::productlist>
         <div v-else class="text-muted">
             @lang('No products selected.')
