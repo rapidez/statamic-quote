95ba90a51dbcf3f6b53e8b568179c627ca61c9c5
Jade Geels <jade@justbetter.nl>
2026-02-03 08:17:31 +0000

    Use withStore to avoid the store being incorrect after augmenting (#13)

--- src/Fieldtypes/Products.php
+++ src/Fieldtypes/Products.php
@@ -27,44 +27,44 @@ class Products extends Fieldtype
     {
         $store = $data['store'] ?? 1;
 
-        Rapidez::setStore($store);
+        return Rapidez::withStore($store, function() use ($data, $store) {
+            $products = collect(json_decode($data['products'] ?? $data, true));
+            $productModel = config('rapidez.models.product');
+            /** @var \Rapidez\Core\Models\Product $productInstance */
+            $productInstance = new $productModel;
+            $dbProducts = $productModel::withoutGlobalScopes()
+                ->whereIn($productInstance->qualifyColumn('sku'), $products->map(fn($product) => $product['sku']))
+                ->get()
+                ->keyBy('sku');
 
-        $products = collect(json_decode($data['products'] ?? $data, true));
-        $productModel = config('rapidez.models.product');
-        /** @var \Rapidez\Core\Models\Product $productInstance */
-        $productInstance = new $productModel;
-        $dbProducts = $productModel::withoutGlobalScopes()
-            ->whereIn($productInstance->qualifyColumn('sku'), $products->map(fn($product) => $product['sku']))
-            ->get()
-            ->keyBy('sku');
+            return $products->map(function($product) use ($dbProducts, $store) {
+                $dbProduct = $dbProducts[$product['sku']] ?? null;
 
-        return $products->map(function($product) use ($dbProducts, $store) {
-            $dbProduct = $dbProducts[$product['sku']] ?? null;
+                $productOptions = $dbProduct
+                    ? collect($product['options'] ?? [])->map(function (string $optionValue, string $option) use ($dbProduct): array {
+                        $optionData = collect($dbProduct->options)->first(fn ($productOption) => $productOption->option_id == $option) ?? null;
+                        $value = collect($optionData?->values ?? [])->first(fn ($value) => $value->option_type_id == $optionValue) ?? null;
 
-            $productOptions = $dbProduct
-                ? collect($product['options'] ?? [])->map(function (string $optionValue, string $option) use ($dbProduct): array {
-                    $optionData = collect($dbProduct->options)->first(fn ($productOption) => $productOption->option_id == $option) ?? null;
-                    $value = collect($optionData?->values ?? [])->first(fn ($value) => $value->option_type_id == $optionValue) ?? null;
+                        return [
+                            'title' => $optionData?->title ?? $option,
+                            'price' => ($value?->price->price ?? 0) + ($optionData?->price->price ?? 0),
+                            'value' => $value ?? ['title' => $optionValue],
+                        ];
+                    })
+                    : null;
 
-                    return [
-                        'title' => $optionData?->title ?? $option,
-                        'price' => ($value?->price->price ?? 0) + ($optionData?->price->price ?? 0),
-                        'value' => $value ?? ['title' => $optionValue],
-                    ];
-                })
-                : null;
+                $totalPrice = $dbProduct
+                    ? ($productOptions->sum('price.price') + $dbProduct->price) * $product['qty']
+                    : null;
 
-            $totalPrice = $dbProduct
-                ? ($productOptions->sum('price.price') + $dbProduct->price) * $product['qty']
-                : null;
-
-            return [
-                ...$product,
-                'store' => $store,
-                'product' => $dbProduct,
-                'options' => $productOptions,
-                'totalPrice' => $totalPrice,
-            ];
+                return [
+                    ...$product,
+                    'store' => $store,
+                    'product' => $dbProduct,
+                    'options' => $productOptions,
+                    'totalPrice' => $totalPrice,
+                ];
+            });
         });
     }
 }
