340945ae0f8b32148e3048bd5d8aaf83a8bd8d20
Jade Geels <jade@justbetter.nl>
2026-02-25 11:39:05 +0100

    Use vue:loaded

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,4 +1,7 @@
 import 'Vendor/rapidez/core/resources/js/vue'
 import { defineAsyncComponent } from 'vue'
 
-Vue.component('quote-data', defineAsyncComponent(() => import('./components/QuoteData.vue')))
+document.addEventListener('vue:loaded', function (event) {
+    const vue = event.detail.vue
+    vue.component('quote-data', defineAsyncComponent(() => import('./components/QuoteData.vue')))
+})
